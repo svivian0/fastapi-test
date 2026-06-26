fc8245a02409b402c37aa480d2a6e2a15cffeec3
Samuel <vivi4n.sam@gmail.com>
2026-06-26 13:25:34 -0300

    add coments

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()# this create an app called "app"
 
-from routes.auth_routes import auth_router
+
+
+
+#this import the routes building in a diferent file
+from routes.auth_routes import auth_router 
 from routes.order_routes import order_router
 
 
+
+#this include the routes in the app
 app.include_router(auth_router)
 app.include_router(order_router)
--- src/models.py
+++ src/models.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
 
 db = create_engine("sqlite:///../data/database.db")
+
+Base = declarative_base()
